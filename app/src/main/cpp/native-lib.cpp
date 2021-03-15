ec580727c0ea64c47b353c21eb8dd98e2862f36e
jamie <jamietohhl@gmail.com>
2021-03-15 17:19:37 +0800

    First Update (Not Working)

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,10 @@
 #include <jni.h>
 #include <string>
+#include <android/sensor.h>
+#include <android/looper.h>
+
+const char packageName[] = "com.example.jnimonitorapplication";
+const int looper = 1;
 
 extern "C" JNIEXPORT jstring JNICALL
 //Java_com_example_jnimonitorapplication_MainActivity_stringFromJNI(
@@ -8,3 +13,52 @@ extern "C" JNIEXPORT jstring JNICALL
 //    std::string hello = "Hello from C++";
 //    return env->NewStringUTF(hello.c_str());
 //}
+
+Java_com_example_jnimonitorapplication_AccelerometerActivity_accelerometerData(
+        JNIEnv* env,
+        jobject /* this */) {
+    ASensorManager* sensorManager = ASensorManager_getInstanceForPackage(packageName);
+    if (!sensorManager) {
+        std::string message = "Failed to get sensor manager";
+        return env->NewStringUTF(message.c_str());
+    }
+
+    ASensorList sensor_list;
+    int sensor_count = ASensorManager_getSensorList(sensorManager, &sensor_list);
+    printf("Found %d sensors\n", sensor_count);
+    for (int i = 0; i < sensor_count; i++) {
+        printf("Found %s\n", ASensor_getName(sensor_list[i]));
+    }
+
+    ASensorEventQueue* queue = ASensorManager_createEventQueue(
+            sensorManager,
+            ALooper_prepare(ALOOPER_PREPARE_ALLOW_NON_CALLBACKS),
+            looper, NULL /* no callback */, NULL /* no data */);
+    if (!queue) {
+        fprintf(stderr, "Failed to create a sensor event queue\n");
+    }
+
+    const int numSamples = 10;
+    const int numEvents = 1;
+    const int timeoutMilliSecs = 1000;
+    //const waitTimeSecs = 1;
+
+    const ASensor* sensor = ASensorManager_getDefaultSensor(sensorManager, ASENSOR_TYPE_ACCELEROMETER);
+    if (sensor && !ASensorEventQueue_enableSensor(queue, sensor)) {
+        int ident = ALooper_pollAll(timeoutMilliSecs, NULL, NULL, NULL);
+
+        for (int i=0; i < numSamples; i++) {
+            int ident = ALooper_pollAll(timeoutMilliSecs, NULL, NULL, NULL);
+            if (ident == looper) {
+                if (sensor != NULL) {
+                    ASensorEvent data;
+                    if (ASensorEventQueue_getEvents(queue, &data, numEvents) > 0) {
+                        std::string dataMessage = std::string("x: ") + std::to_string(data.acceleration.y) +std::string("\ny: ")+ std::to_string(data.acceleration.y)+std::string("\nz: ")+ std::to_string(data.acceleration.y);
+                        return env->NewStringUTF(dataMessage.c_str());
+                    }
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/jnimonitorapplication/AccelerometerActivity.java
+++ app/src/main/java/com/example/jnimonitorapplication/AccelerometerActivity.java
@@ -3,18 +3,25 @@ package com.example.jnimonitorapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AccelerometerActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("sensors.h");
     }
+
+    public native String accelerometerData();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(accelerometerData());
+
     }
 
     /**
@@ -22,4 +29,9 @@ public class AccelerometerActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     //public native String stringFromJNI();
+
+
+
+
+
 }
